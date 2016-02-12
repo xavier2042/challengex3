8dd7813d5f8b8f02aa1a80881c2e5e5f42063b5b
Médéric Voisin <mederic.medo@gmail.com>
2016-02-12 11:57:37 +0100

    tout plein de chose... :p

--- js/panier.js
+++ js/panier.js
@@ -1,33 +1,47 @@
-function afficherPanier() {
-  console.log(panier);
-  panier = JSON.parse(sessionStorage.panierSauvegarde);
+function affichePanier() {
+
+  var panier = JSON.parse(sessionStorage.paniersauvegarde);
+
   for (var i = 0; i < panier.length; i++) {
+
     var elementPanier = document.getElementById("panier");
     var creationTr = document.createElement("tr");
     elementPanier.appendChild(creationTr);
-    // image
-    var creatrionTd = document.createElement("td");
-    creationTr.appendChild(creatrionTd);
-    var creationImg = document.createElement("img");
-    creationTd.appendChild(creationImg);
-    creationImg.src = ""; // soit panier soit catalogue
-    // nom du livre
-    var creatrionTd = document.createElement("td");
-    creationTr.appendChild(creatrionTd);
-    creationTd.innerHTML = "";
-    // prix unitaire
-    var creatrionTd = document.createElement("td");
-    creationTr.appendChild(creatrionTd);
-    creationTd.innerHTML = "";
-    // nombre d'article
-    var creatrionTd = document.createElement("td");
-    creationTr.appendChild(creatrionTd);
-    creationTd.innerHTML = "";
-    // prix total
-    var creatrionTd = document.createElement("td");
-    creationTr.appendChild(creatrionTd);
-    creationTd.innerHTML = "";
 
-    creationTr.innerHTML = panier[i];
-  }
+    // image
+    var creationTd1 = document.createElement("td");
+    var creationImg = document.createElement("img");
+    creationTr.appendChild(creationTd1);
+    creationImg.src = panier[i].image;
+    creationTd1.appendChild(creationImg);
+
+    // nom du livre
+    var creationTd2 = document.createElement("td");
+    creationTr.appendChild(creationTd2);
+    creationTd2.innerHTML = panier[i].nom;
+    creationTd2.style.lineHeight = 10;
+    creationTd2.style.fontWeight = "bold";
+
+    // prix unitaire
+    var creationTd3 = document.createElement("td");
+    creationTr.appendChild(creationTd3);
+    creationTd3.innerHTML = panier[i].prix + " Euros";
+    creationTd3.style.lineHeight = 10;
+    creationTd3.style.fontWeight = "bold";
+
+    // nombre d'article
+    var creationTd4 = document.createElement("td");
+    creationTr.appendChild(creationTd4);
+    creationTd4.innerHTML = panier[i].quantite;
+    creationTd4.style.lineHeight = 10;
+    creationTd4.style.fontWeight = "bold";
+
+    // prix total
+    var creationTd5 = document.createElement("td");
+    creationTr.appendChild(creationTd5);
+    creationTd5.innerHTML = (panier[i].prix * panier[i].quantite) + " Euros";
+    creationTd5.style.lineHeight = 10;
+    creationTd5.style.fontWeight = "bold";
+    }
+
 }
